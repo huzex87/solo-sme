1895d94ef4f5152eee50db084d1325cc3741971c
Huzaifa Yakubu Musa <huzex87@gmail.com>
2026-03-12 05:38:14 +0100

    fix: exhaustive Sentry v10 config fixes for Next.js 16 build stability

--- next.config.ts
+++ next.config.ts
@@ -48,16 +48,16 @@ export default withSentryConfig(nextConfig, {
 
   // Sourcemap configuration
   sourcemaps: {
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+    // Automatically delete client-side source maps after uploading them to Sentry.
+    deleteSourcemapsAfterUpload: true,
   },
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  // Note: These webpack-specific settings are ignored by Turbopack but kept for compatibility.
+  wepack: {
+    automaticVercelMonitors: true,
+  },
+  treeshake: {
+    removeDebugLogging: true,
+  },
 });
